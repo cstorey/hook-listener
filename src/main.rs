40cdd0b9b19acd731e9f9c58abe354a390c9746d
Ceri Storey <cez@necrofish.org.uk>
2019-06-09 16:04:37 +0100

    Verify signatures.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::collections::BTreeMap;
 
-use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
+use actix_web::{error, http, middleware, web, App, Error, HttpResponse, HttpServer};
 use failure::Fallible;
 use futures::stream::Stream;
 use futures::Future;
+use hex::FromHex;
 use log::*;
 use pg_queue::Producer;
 use r2d2::Pool;
 use r2d2_postgres::{PostgresConnectionManager, TlsMode};
+use ring::hmac;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -17,14 +19,64 @@ struct Opt {
     bind: std::net::SocketAddr,
     #[structopt(short = "p", long = "pgsql")]
     postgresql: String,
+    #[structopt(short = "s", long = "secret-key")]
+    secret: String,
+}
+
+#[derive(Debug, Clone)]
+struct Verifier {
+    key: Vec<u8>,
+}
+
+impl Verifier {
+    fn check(&self, body: &[u8], value: &http::HeaderValue) -> Result<(), Error> {
+        let mut it = value.as_bytes().split(|c| *c == b'=');
+        let algo_name = it
+            .next()
+            .ok_or_else(|| error::ErrorBadRequest("No algorithm found"))?;
+        debug!(
+            "hub signature algorithm: {:?}",
+            String::from_utf8_lossy(&algo_name)
+        );
+        let sig = it
+            .next()
+            .ok_or_else(|| error::ErrorBadRequest("No signature found"))
+            .and_then(|s| {
+                Vec::from_hex(s)
+                    .map_err(|e| error::ErrorBadRequest(format!("Parsing signature: {}", e)))
+            })?;
+        debug!("hub signature value: {:?}", String::from_utf8_lossy(&sig));
+        if it.next().is_some() {
+            return Err(error::ErrorBadRequest("More than one '='"));
+        }
+
+        let algo = match algo_name {
+            b"sha1" => &ring::digest::SHA1,
+            other => {
+                return Err(error::ErrorBadRequest(format!(
+                    "Invalid hmac method {}",
+                    String::from_utf8_lossy(other)
+                )))
+            }
+        };
+        let key = hmac::VerificationKey::new(algo, &self.key);
+
+        hmac::verify(&key, body, &sig).map_err(|e| {
+            error!("Verifying signature: {}", e);
+            error::ErrorBadRequest("Invalid signature")
+        })?;
+
+        Ok(())
+    }
 }
 
 /// async handler
 fn ingest(
-    (path, body, pool, req): (
+    (path, body, pool, verifier, req): (
         web::Path<String>,
         web::Payload,
         web::Data<r2d2::Pool<r2d2_postgres::PostgresConnectionManager>>,
+        web::Data<Verifier>,
         web::HttpRequest,
     ),
 ) -> impl Future<Item = HttpResponse, Error = Error> {
@@ -46,6 +98,15 @@ fn ingest(
             body.extend_from_slice(&chunk);
             Ok::<_, Error>(body)
         })
+        .and_then(move |bytes| {
+            let sig = req
+                .headers()
+                .get("x-hub-signature")
+                .ok_or_else(|| error::ErrorBadRequest("missing x-hub-signature"))?;
+            debug!("x-hub-signature: {:?}", sig);
+            verifier.check(&bytes, sig)?;
+            Ok(bytes)
+        })
         .and_then(|bytes| {
             let json = serde_json::from_slice::<serde_json::Value>(&bytes)?;
             Ok(json)
@@ -66,6 +127,11 @@ fn ingest(
 fn main() -> Fallible<()> {
     env_logger::init();
     let opt = Opt::from_args();
+
+    let verifier = Verifier {
+        key: opt.secret.as_bytes().to_vec(),
+    };
+
     let pool = Pool::builder()
         .max_size(1)
         .build(PostgresConnectionManager::new(
@@ -79,6 +145,7 @@ fn main() -> Fallible<()> {
         App::new()
             // enable logger - always register actix-web Logger middleware last
             .wrap(middleware::Logger::default())
+            .data(verifier.clone())
             .data(pool.clone())
             // with path parameters
             .service(web::resource("/gh/{path:.*}").route(web::post().to_async(ingest)))
